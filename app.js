0fdc2218a4465a62b0f063865a9bde95940c83c0
mazaherm <mmuraj.313@gmail.com>
2018-10-02 09:58:41 +0100

    about me dynamic data

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const port = 8000
 
 app.set('view engine', 'pug');
 app.get('/', (req, res) => res.render("index"))
-app.get('/about-me', (req, res) => res.render("index"))
+
+app.get('/about-me', (req, res) => res.render("index", {
+    firstname: "Mazaher",
+    lastname: "Muraj",
+    company: "MMT",
+    position: "Junior React Developer",
+    likes: ["Footballs", "Films", "React"]
+}))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
