12626caca2a6cd05acd96840df5a7659bc442524
mazaherm <mmuraj.313@gmail.com>
2018-10-02 11:47:17 +0100

    logger function - middleware test

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render("index"))
 app.get('/about-me', (req, res) => res.render("about-me"))
 
+const logger = (req, res, next) => {
+    console.log(new Date(), req.method, req.url);
+    next();
+}
+
+app.use(logger);
+
 app.listen(port, () => console.log(`Listening on port ${port}`))
