f57923483655fe6b5f3582104baa3413fffd1a6c
mazaherm <mmuraj.313@gmail.com>
2018-10-02 10:12:07 +0100

    new page about me

--- app.js
+++ app.js
@@ -3,14 +3,8 @@ const app = express()
 const port = 8000
 
 app.set('view engine', 'pug');
-app.get('/', (req, res) => res.render("index"))
 
-app.get('/about-me', (req, res) => res.render("index", {
-    firstname: "Mazaher",
-    lastname: "Muraj",
-    company: "MMT",
-    position: "Junior React Developer",
-    likes: ["Footballs", "Films", "React"]
-}))
+app.get('/', (req, res) => res.render("index"))
+app.get('/about-me', (req, res) => res.render("about-me"))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
