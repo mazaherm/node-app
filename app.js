448d76bfb23d2d8de61a849c04656c336fc8acac
mazaherm <mmuraj.313@gmail.com>
2018-10-02 10:34:58 +0100

    Tell express to use static js and css files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 8000
 
 app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.render("index"))
 app.get('/about-me', (req, res) => res.render("about-me"))
