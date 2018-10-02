4aa01d8bb04db4e0cd6b33a68f479df9cefb6f7c
mazaherm <mmuraj.313@gmail.com>
2018-10-02 12:09:16 +0100

    error handler for unknown request

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const logger = (req, res, next) => {
     next();
 }
 
+const errorMessage = (req, res, next) => {
+    console.log("Oh no! Page not found");
+    res.send(404, "Oh no! Page not found");
+}
+
 app.use(logger);
+app.use(errorMessage);
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
