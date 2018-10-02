73b9d795412bb298f524a3a0436aa14d5e88aade
mazaherm <mmuraj.313@gmail.com>
2018-10-02 09:21:47 +0100

    pug added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+app.set('view engine', 'pug');
 app.get('/', (req, res) => res.send("Hello World!"))
 
-app.listen(port, () => console.log(`Listining on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`))
