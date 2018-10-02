98c96ffb49e0df075abdffd4ba78560d39709d4f
mazaherm <mmuraj.313@gmail.com>
2018-10-02 14:47:53 +0100

    commented out pug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.set('view engine', 'pug');
+//app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.render("index"))
